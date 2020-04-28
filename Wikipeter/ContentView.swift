79cbad116901a7d2738f00b2db037a89b772f6ff
Adrian <hannsadrian@gmail.com>
2020-04-28 09:42:01 +0200

    Initial Commit

---
+++ Wikipeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wikipeter
+//
+//  Created by Adrian Böhme on 28.04.20.
+//  Copyright © 2020 Adrian Böhme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
